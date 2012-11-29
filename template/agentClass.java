46d3957b80576e1ede0df5be573f73dcd57c9df1
rkempter <renato.kempter@gmail.com>
2012-11-29 15:13:06 +0100

    Added basic private variables

--- template/agentClass.java
+++ template/agentClass.java
@@ -2,20 +2,39 @@ package template;
 
 import java.util.ArrayList;
 
+import logist.simulation.Vehicle;
+import logist.task.Task;
+
 public class agentClass {
 	
 	// Total cost last time
 	
-	// List of tasks
+	private Vehicle vehicle;
 	
-	// List of oponents
+	// List of tasks for this agent
+	private ArrayList<Task> taskList = new ArrayList<Task>();
 	
-	// Arraylist with tasks of opponents
+	// Arraylist with task of opponents
+	private ArrayList<ArrayList<Task>> opponentTaskList = new ArrayList<ArrayList<Task>>();
 	
-	// Arraylist with estimated cost of opponents
+	// Arraylist with costs of opponents
+	
+	
+	public agentClass(Vehicle _vehicle) {
+		this.vehicle = _vehicle;
+	}
 	
 	public static boolean checkGoalState(ArrayList<ArrayList<Object>> states) {
 		return true;
 	}
 	
+	public void addTask(Task task) {
+		taskList.add(task);
+	}
+	
+	public int computeCosts() {
+		
+		
+		return 0;
+	}
 }
